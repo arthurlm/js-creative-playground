ee4da972de15df5ea5f36e6769de99d5f8cbb229
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2022-02-26 17:28:34 +0100

    Add palette to scene 3

--- src/scenes/scene3.ts
+++ src/scenes/scene3.ts
@@ -1,20 +1,24 @@
+import { Hsla, Palette } from "../colors";
 import { Context, Scene } from "../context";
 import { Point2, Polygon } from "../geometry";
 import { randRange } from "../math";
 import {
   CompositeOscillator,
   ConstOscillator,
-  LinearOscillator,
   Oscillator,
   SineOscilator,
 } from "../oscillator";
+
+const palette = new Palette(
+  "54478c-2c699a-048ba8-0db39e-16db93-83e377-b9e769-efea5a-f1c453-f29e4c"
+);
 
 const scene = new Scene();
 scene.frameOpacity = 0.05;
 
 class AnimatedPolygon extends Polygon {
   radiusRatio: number;
-  hue: number;
+  color: Hsla;
   lineWidth: number;
   angleOscillator: Oscillator;
 
@@ -22,7 +26,7 @@ class AnimatedPolygon extends Polygon {
     super();
 
     this.radiusRatio = 20.0;
-    this.hue = (context.tickCount / 2) % 360;
+    this.color = palette.getColor(context.tickCount / 100).toHsla();
     this.lineWidth = randRange(0, 5);
 
     this.angleOscillator = new CompositeOscillator([
@@ -45,7 +49,7 @@ class AnimatedPolygon extends Polygon {
   }
 
   drawBegin(context: Context): void {
-    context.ctx.strokeStyle = `hsl(${this.hue}, 40%, 65%)`;
+    context.ctx.strokeStyle = this.color.toString();
     context.ctx.lineWidth = this.lineWidth;
   }
 }
