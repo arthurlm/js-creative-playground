f7da45c5edb8d3b6ce4fa87001fa418615e149a6
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2022-02-26 09:34:31 +0100

    Add interpolate functions

--- src/math.ts
+++ src/math.ts
@@ -41,9 +41,39 @@ export function radToDeg(value: number): number {
   return (value * 180) / Math.PI;
 }
 
+/**
+ * Cosine interpolation.
+ *
+ * @param y0 y0 value
+ * @param y1 y1 value
+ * @param x x value
+ * @returns Y between Y0 and Y1
+ */
+export function cosineInterpolate(y0: number, y1: number, x: number): number {
+  const ft = x * Math.PI;
+  const f = (1.0 - Math.cos(ft)) * 0.5;
+  return y0 * (1.0 - f) + y1 * f;
+}
+
+/**
+ * Linear interpolation.
+ *
+ * @param y0 y0 value
+ * @param y1 y1 value
+ * @param x x value
+ * @returns Y between Y0 and Y1
+ *
+ * @see https://en.wikipedia.org/wiki/Linear_interpolation
+ */
+export function lerp(y0: number, y1: number, x: number): number {
+  return (1.0 - x) * y0 + x * y1;
+}
+
 export default {
   clip,
   randRange,
   radToDeg,
   degToRad,
+  cosineInterpolate,
+  lerp,
 };
